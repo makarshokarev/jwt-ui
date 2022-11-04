a89a845da53ff8aa987cda07bc710d0f41e0ddba
makarshokarev <makarshokarev@gmail.com>
2022-11-04 08:04:43 +0200

    initial commit

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,52 @@
+import {Injectable} from "@angular/core";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {UserAuthService} from "./user-auth.service";
+
+@Injectable({
+  providedIn: "root"
+})
+export class UserService {
+
+  PATH_OF_API = "http://localhost:9090";
+  requestHeader = new HttpHeaders(
+    {"No-Auth": "True"}
+  );
+
+  constructor(private httpClient: HttpClient,
+              private userAuthService: UserAuthService) {
+  }
+
+  public login(loginData: any) {
+    return this.httpClient.post(this.PATH_OF_API + "/authenticate", loginData, {headers: this.requestHeader});
+  }
+
+  public forUser() {
+    return this.httpClient.get(this.PATH_OF_API + "/forUser", {
+      responseType: "text",
+    });
+  }
+
+  public forAdmin() {
+    return this.httpClient.get(this.PATH_OF_API + "/forAdmin", {
+      responseType: "text",
+    });
+  }
+
+  public roleMatch(allowedRoles: string []): boolean {
+    let isMatch = false;
+    const userRoles: any = this.userAuthService.getRoles();
+
+    if (userRoles != null && userRoles) {
+      for (let i = 0; i < userRoles.length; i++) {
+        for (let j = 0; j < allowedRoles.length; j++) {
+          if (userRoles[i].roleName === allowedRoles[j]) {
+            isMatch = true;
+            return isMatch;
+          }
+        }
+      }
+    }
+
+    return isMatch;
+  }
+}
